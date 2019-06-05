e12ce211204542f03643c33467d0631a1bd2e263
rksp <riki.syahputra@defenxor.com>
2019-06-05 17:09:32 +0700

    added firebase cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,14 +8,37 @@ exports.newSubscriberNotification = functions.firestore
     .onCreate( async event => {
         const data = event.data();
 
-        const userId = data['userId'];
-        const subscriber = data['subscriberId'];
+        if(data) {
+            const userId = data.userId;
+            const subscriber = data.subscriberId;
 
-        const payload  = {
-            notification: {
-                title: 'New Subscriber',
-                body: `${subscriber} is following your content`,
-                icon: 'https://goo.gl/Fz9nrQ'
+            const payload  = {
+                notification: {
+                    title: 'New Subscriber',
+                    body: `${subscriber} is following your content`,
+                    icon: 'https://goo.gl/Fz9nrQ'
+                }
             }
+
+            const db = admin.firestore();
+            const devicesRef = db.collection('devices').where('userId',  '==', userId);
+
+            const devices = await devicesRef.get();
+            const tokens: string[] = [];
+
+            if(devices) {
+                devices.forEach(result => {
+                    const token = result.data().token;
+                    tokens.push(token);
+                });
+    
+                return admin.messaging().sendToDevice(tokens, payload);
+            } else {
+                console.log('no user for that id found');
+                return;
+            }
+        } else {
+            console.log('no data');
+            return;
         }
     })
